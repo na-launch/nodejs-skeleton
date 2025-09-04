b8d6fa9b04da8a56d73f74edbc4902459b68e19f
pkredhat <120052595+pkredhat@users.noreply.github.com>
2025-09-04 09:41:45 -0400

    misc changes

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ WORKDIR /usr/src/app
 COPY package*.json ./
 RUN npm install --only=production
 COPY . .
-EXPOSE ${{ values.port }}
+EXPOSE 3000
 CMD ["npm", "start"]
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || ${{ values.port }};
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello from Node.js app scaffolded by DevHub!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
