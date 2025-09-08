a9847c5ec7370fb5fc27a33276e78e48c38ff976
pkredhat <120052595+pkredhat@users.noreply.github.com>
2025-09-08 09:22:24 -0400

    added branch env var

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js app scaffolded by DevHub!');
+  res.send(`Hello from Node.js app scaffolded by DevHub! (You are currently on branch: <strong>${BRANCH}</strong>). <em>This is Revision 101</em>`);
 });
 
 app.listen(PORT, "0.0.0.0", () => {
